3fb0a88ccc181b6c46ea7053d99c7906ff9f0254
Nabeel Ali Zeb <45977499+nabeelzeb@users.noreply.github.com>
2022-05-31 13:16:37 +0200

    Ny index/Filoverførsel

--- flask-app1/app/views.py
+++ flask-app1/app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView, ModelRestApi
-
+from flask_appbuilder import ModelView, CompactCRUDMixin
+from app.models import Project, ProjectFiles
 from . import appbuilder, db
 
 """
@@ -63,7 +63,7 @@ class DemoView(BaseView):
     def page2(self, message):
         '''API with parameter + customized view'''
         message = f'message #2: {message}'
-        return render_template('method1.html', message=message)
+        return render_template('method1.html', message=message, base_template=appbuilder.base_template, appbuilder=appbuilder)
 
     @expose('/method3/')
     @has_access
@@ -92,5 +92,40 @@ def page_not_found(e):
         404,
     )
 
+class ProjectFilesModelView(ModelView):
+    datamodel = SQLAInterface(ProjectFiles)
+
+    label_columns = {"file_name": "File Name", "download": "Download"}
+    add_columns = ["file", "description", "project"]
+    edit_columns = ["file", "description", "project"]
+    list_columns = ["file_name", "download"]
+    show_columns = ["file_name", "download"]
+
+
+class ProjectModelView(CompactCRUDMixin, ModelView):
+    datamodel = SQLAInterface(Project)
+    related_views = [ProjectFilesModelView]
+
+    show_template = "appbuilder/general/model/show_cascade.html"
+    edit_template = "appbuilder/general/model/edit_cascade.html"
+
+    add_columns = ["name"]
+    edit_columns = ["name"]
+    list_columns = ["name", "created_by", "created_on", "changed_by", "changed_on"]
+    show_fieldsets = [
+        ("Info", {"fields": ["name"]}),
+        (
+            "Audit",
+            {
+                "fields": ["created_by", "created_on", "changed_by", "changed_on"],
+                "expanded": False,
+            },
+        ),
+    ]
+
 
 db.create_all()
+appbuilder.add_view(
+    ProjectModelView, "List Projects", icon="fa-table", category="Projects"
+)
+appbuilder.add_view_no_menu(ProjectFilesModelView)
